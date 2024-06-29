17b8783d66925d7707d3d6e4571aeab3c89557e5
Subhashinibalu <subhashiniulk@gmail.com>
2024-06-29 14:21:47 +0530

    job recommendation check

--- src/Pages/Job.jsx
+++ src/Pages/Job.jsx
@@ -2,7 +2,6 @@ import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { mycontext } from "../App";
 import { Button, Toast } from "flowbite-react";
-
 import { MdLoop } from "react-icons/md";
 import { Link } from "react-router-dom";
 
@@ -12,6 +11,7 @@ const Job = () => {
   const [user, setUser] = useContext(mycontext);
   const token = localStorage.getItem("Token");
   const [applied, setApplied] = useState([]);
+  const [recommend, setRecommend] = useState(false);
   useEffect(() => {
     fetchData();
   }, []);
@@ -21,7 +21,8 @@ const Job = () => {
       `https://jobssearchbackend.onrender.com/api/admin/getjobs/${user._id}`
     );
     if (response.status == 200) {
-      setJobs(response.data); //response is set to jobs
+      setJobs(response.data); 
+      setRecommend(true);//response is set to jobs
     } else {
       console.log(response.data.message);
     }
@@ -49,109 +50,109 @@ const Job = () => {
   };
 
   return (
-    <>
-      <div className=" p-10 min-h-screen overflow-auto ">
-        {jobs?jobs.map((ele, index) => {
-          return (
-            <div key={index}>
-              <form onSubmit={handleSubmit}>
-                <div className="space-y-12 p-4 m-2 hover:shadow-xl">
-                  <div className="border border-gray-900/10 rounded-lg mx-auto bg-gradient-to-r from-indigo-400 to-cyan-400 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-6 p-12">
-                    {/* for company name  and link */}
-                    <div className="md:col-span-2 sm:col-start-1 mt-5">
-                      <h1 className="text-center mb-3 mt-5 md:text-4xl fond-bold sm:text-xl font-serif">
-                        {ele.company}
-                      </h1>
-                      <a
-                        href={ele.link}
-                        target="_blank"
-                        className="md:text-center overflow-hidden block hover:text-transparent hover:bg-clip-text  hover:bg-gradient-to-br from-blue-200 to-cyan-200 text-gray-800"
-                      >
-                        <i>{ele.link}</i>
-                      </a>
-                    </div>
+    <>{ recommend ?  <div className=" p-10 max-h-screen overflow-auto ">
+      {jobs.map((ele, index) => {
+        return (
+          <div key={index}>
+            <form onSubmit={handleSubmit}>
+              <div className="space-y-12 p-4 m-2 hover:shadow-xl ">
+                <div className="border border-gray-900/10 rounded-lg mx-auto bg-gradient-to-r from-indigo-400 to-cyan-400 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-6 p-12">
+                  {/* for company name  and link */}
+                  <div className="md:col-span-2 sm:col-start-1 mt-5">
+                    <h1 className="text-center mb-3 mt-5 md:text-4xl fond-bold sm:text-xl font-serif">
+                      {ele.company}
+                    </h1>
+                    <a
+                      href={ele.link}
+                      target="_blank"
+                      className="md:text-center overflow-hidden block hover:text-transparent hover:bg-clip-text  hover:bg-gradient-to-br from-blue-200 to-cyan-200 text-gray-800"
+                    >
+                      <i>{ele.link}</i>
+                    </a>
+                  </div>
 
-                    {/* for role and salary*/}
-                    <div className="md:col-span-1 p-3 font-serif">
-                      <p className="text-center ">
-                        Role:
-                        <br />
-                        <span className="shadow-md mt-5">{ele.role}</span>
-                      </p>
-                      <p className="text-center mt-8 ">
-                        Package:
-                        <br />
-                        <span className="shadow-md mt-1">{ele.salary}</span> /yr
-                      </p>
-                    </div>
-                    {/* for location and skills*/}
-                    <div className="md:col-span-1 p-3  font-serif">
-                      <p className="text-center ">
-                        Location:
-                        <br />
-                        <span className="shadow-md mt-1">{ele.location}</span>
-                      </p>
-                      <p className="text-center mt-8 overflow-x-auto ">
-                        Skills:
-                        <br />
-                        <span className="shadow-md mt-1 ">
-                          {ele.skillsRequired}
-                        </span>
-                      </p>
-                    </div>
-                    {/* to apply*/}
-                    <div className="md:col-span-1 p-3  font-serif flex justify-center items-center">
-                      <p className="text-center  overflow-hidden ">
-                        Mail us:
-                        <br />
-                        <span className="shadow-md mt-1 ">{ele.email}</span>
-                      </p>
-                    </div>
-                    <div className="md:col-span-1 p-4 ms-10 font-serif flex justify-center items-center">
-                      <input
-                        type="text"
-                        className="hidden"
-                        value={ele._id}
-                        readOnly
-                      />
-                      {user.applications.includes(`${ele._id}`) ? (
-                        <Button className="bg-green-700" disabled>
-                          APPLIED
-                        </Button>
-                      ) : (
-                        <Button
-                          className="bg-blue-700"
-                          type="submit"
-                          onClick={() => handleUpdate(index)}
-                        >
-                          APPLY
-                        </Button>
-                      )}
-                    </div>
+                  {/* for role and salary*/}
+                  <div className="md:col-span-1 p-3 font-serif">
+                    <p className="text-center ">
+                      Role:
+                      <br />
+                      <span className="shadow-md mt-5">{ele.role}</span>
+                    </p>
+                    <p className="text-center mt-8 ">
+                      Package:
+                      <br />
+                      <span className="shadow-md mt-1">{ele.salary}</span> /yr
+                    </p>
+                  </div>
+                  {/* for location and skills*/}
+                  <div className="md:col-span-1 p-3  font-serif">
+                    <p className="text-center ">
+                      Location:
+                      <br />
+                      <span className="shadow-md mt-1">{ele.location}</span>
+                    </p>
+                    <p className="text-center mt-8 overflow-x-auto ">
+                      Skills:
+                      <br />
+                      <span className="shadow-md mt-1 ">
+                        {ele.skillsRequired}
+                      </span>
+                    </p>
+                  </div>
+                  {/* to apply*/}
+                  <div className="md:col-span-1 p-3  font-serif flex justify-center items-center">
+                    <p className="text-center  overflow-hidden ">
+                      Mail us:
+                      <br />
+                      <span className="shadow-md mt-1 ">{ele.email}</span>
+                    </p>
+                  </div>
+                  <div className="md:col-span-1 p-4 ms-10 font-serif flex justify-center items-center">
+                    <input
+                      type="text"
+                      className="hidden"
+                      value={ele._id}
+                      readOnly
+                    />
+                    {user.applications.includes(`${ele._id}`) ? (
+                      <Button className="bg-green-700" disabled>
+                        APPLIED
+                      </Button>
+                    ) : (
+                      <Button
+                        className="bg-blue-700"
+                        type="submit"
+                        onClick={() => handleUpdate(index)}
+                      >
+                        APPLY
+                      </Button>
+                    )}
                   </div>
                 </div>
-              </form>
-            </div>
-          );
-        }):       <Toast>
-        <div className="flex items-start">
-          <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-cyan-100 text-cyan-500 dark:bg-cyan-900 dark:text-cyan-300">
-            <MdLoop className="h-5 w-5" />
-          </div>
-          <div className="ml-3 text-sm font-normal">
-            <span className="mb-1 text-sm font-semibold text-gray-900 dark:text-white">Update Profile</span>
-            <div className="mb-2 text-sm font-normal">To get job recommendation need to update your details.</div>
-            <div className="flex gap-2">
-              <div className="w-auto">
-                <Button size="xs"><Link to='/home'>Update</Link></Button>
               </div>
-              
-            </div>
+            </form>
           </div>
-          <Toast.Toggle />
+        );
+      })}
+    </div> :   <Toast>
+      <div className="flex items-start">
+        <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-cyan-100 text-cyan-500 dark:bg-cyan-900 dark:text-cyan-300">
+          <MdLoop className="h-5 w-5" />
         </div>
-      </Toast>}
+        <div className="ml-3 text-sm font-normal">
+          <span className="mb-1 text-sm font-semibold text-gray-900 dark:text-white">Update Profile</span>
+          <div className="mb-2 text-sm font-normal">To get job recommendations kindly fill out the user details.</div>
+          <div className="flex gap-2">
+            <div className="w-auto">
+              <Button size="xs"><Link to='/home'>Update</Link></Button>
+            </div>
+           
+          </div>
+        </div>
+        <Toast.Toggle />
       </div>
+    </Toast>}
+     
     </>
   );
 };
