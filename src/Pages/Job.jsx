009ccc648ef89b695c3dd105e0f4c4f74f9bc090
Subhashinibalu <subhashiniulk@gmail.com>
2024-06-29 14:26:13 +0530

    Banner added

--- src/Pages/Job.jsx
+++ src/Pages/Job.jsx
@@ -1,9 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { mycontext } from "../App";
-import { Button, Toast } from "flowbite-react";
-import { MdLoop } from "react-icons/md";
+import { Banner, Button, Toast } from "flowbite-react";
+import { MdAnnouncement, MdLoop } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { HiX } from "react-icons/hi";
 
 // this page is for job recommendations based on users skills
 const Job = () => {
@@ -134,24 +135,28 @@ const Job = () => {
           </div>
         );
       })}
-    </div> :   <Toast>
-      <div className="flex items-start">
-        <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-cyan-100 text-cyan-500 dark:bg-cyan-900 dark:text-cyan-300">
-          <MdLoop className="h-5 w-5" />
+    </div> :  <div className=" p-10 max-h-screen overflow-auto ">  <Banner>
+      <div className="flex w-full justify-between border-b border-gray-200 bg-gray-50 p-4 dark:border-gray-600 dark:bg-gray-700">
+        <div className="mx-auto flex items-center">
+          <p className="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+            <MdAnnouncement className="mr-4 h-4 w-4" />
+            <span className="[&_p]:inline">
+              New brand identity has been launched for the&nbsp;
+              <a
+                href="https://flowbite.com"
+                className="inline font-medium text-cyan-600 underline decoration-solid underline-offset-2 hover:no-underline dark:text-cyan-500"
+              >
+                Flowbite Library
+              </a>
+            </span>
+          </p>
         </div>
-        <div className="ml-3 text-sm font-normal">
-          <span className="mb-1 text-sm font-semibold text-gray-900 dark:text-white">Update Profile</span>
-          <div className="mb-2 text-sm font-normal">To get job recommendations kindly fill out the user details.</div>
-          <div className="flex gap-2">
-            <div className="w-auto">
-              <Button size="xs"><Link to='/home'>Update</Link></Button>
-            </div>
-           
-          </div>
-        </div>
-        <Toast.Toggle />
+        <Banner.CollapseButton color="gray" className="border-0 bg-transparent text-gray-500 dark:text-gray-400">
+          <HiX className="h-4 w-4" />
+        </Banner.CollapseButton>
       </div>
-    </Toast>}
+    </Banner>
+    </div>}
      
     </>
   );
